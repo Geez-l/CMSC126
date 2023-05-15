4bba10c42114ad5e57c8d51475b45ac0045a00d2
Geez-l <93700998+Geez-l@users.noreply.github.com>
2023-05-15 12:25:32 +0800

    Create lab6.php

---
+++ Laboratory6/lab6.php
@@ -0,0 +1,15 @@
+<script>
+	Fetch ( URL )
+	https:pokeai.com/v2/pokemon/ditto/abilities
+	pokemon/ditto
+
+	.then (result => {
+		Return result.json()
+	}>.then (data => {
+
+	data.forEach (user =>
+	Const disp += `<li> $user.name </li>;
+
+	//console.log
+	//document.getElementById(“name”).innerHTML= disp;
+</script>
